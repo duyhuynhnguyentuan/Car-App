0d7f253901c4c189df0a74d51cd7a84138ea41d0
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-12-11 22:29:36 +0700

    hamburger menu

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
-import { Slide as Menu } from "react-burger-menu";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive"
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyles";
 const ListContainer = styled.ul`
 ${tw`
     flex
@@ -10,7 +13,7 @@ ${tw`
 `};
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{menu? : any}>`
 ${tw`
     text-xs
     md:text-base
@@ -24,9 +27,41 @@ ${tw`
     ease-in-out
     hover:text-gray-700
 `};
+${({menu}) =>
+    menu && css`
+     ${tw`
+     text-white
+     text-xl
+     mb-3
+     focus:text-white
+     `}
+   `
+}
 `;
 
 export function NavItems() {
+    
+    const isMobile = useMediaQuery({maxWidth: SCREENS.sm })
+    if(isMobile){
+         return(
+        <Menu right styles={menuStyles}>
+            <ListContainer>
+            <NavItem menu>
+                <a href="#">Home</a>
+            </NavItem>
+            <NavItem menu>
+                <a href="#">Cars</a>
+            </NavItem>
+            <NavItem menu>
+                <a href="#">Services</a>
+            </NavItem>
+            <NavItem menu>
+                <a href="#">Contact us</a>
+            </NavItem>
+            </ListContainer>
+        </Menu>
+         )
+    }
     return <ListContainer>
         <NavItem>
             <a href="#">Home</a>
