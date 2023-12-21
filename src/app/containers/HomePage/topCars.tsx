00fbec86713f7d7738a0563fe0726f4547fa6fe9
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-12-21 22:15:55 +0700

    car typing and init topCars

---
+++ src/app/containers/HomePage/topCars.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro"; 
+
+const TopCarsContainer = styled.div`
+${tw`
+max-w-screen-lg
+w-full
+flex
+flex-col
+items-center
+justify-center
+px-4 
+md:px-0  
+mb-10 
+`}
+`
+
+const Title = styled.h1`
+  ${tw`
+    text-black
+    text-2xl
+    md:text-5xl
+    font-extrabold
+    md:font-black
+    md:leading-normal
+  `};
+`;
+
+const CarsContainer = styled.div`
+${tw`
+    w-full
+    flex
+    flex-wrap
+    justify-center
+    mt-7 
+    md:mt-10 
+`}
+`
+
+
+export function TopCars(){
+    return <TopCarsContainer>
+        <Title>Explore our top deals</Title>
+        <CarsContainer>
+            
+        </CarsContainer>
+    </TopCarsContainer>
+}
