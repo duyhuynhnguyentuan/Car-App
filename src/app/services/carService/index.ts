adaf2bdefdffb77d54f3a0481a48d3b7fc17cafd
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2024-01-02 17:08:45 +0700

    setting up apollo graphQL with React

---
+++ src/app/services/carService/index.ts
@@ -0,0 +1,4 @@
+class CarService{
+
+}
+export default new CarService();
---
+++ src/app/services/carService/queries.ts
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+
+export const GET_ALL_CARS = gql`
+query
+{
+    cars{
+        id
+        name
+        dailyPrice
+        monthlyPrice
+        mileage
+        gas
+        gearType
+        thumbnailUrl
+      }
+}
+`
